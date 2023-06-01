5bb8f50a666c9c2d85e7c5c85b7497ac8ae4158e
Danielpark1239 <Danielpark1239@gmail.com>
2023-05-31 23:12:23 -0400

    Basic images router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { exampleRouter } from "~/server/api/routers/example";
+import { imagesRouter } from "~/server/api/routers/images";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  images: imagesRouter,
 });
 
 // export type definition of API
--- src/server/api/routers/images.ts
+++ src/server/api/routers/images.ts
@@ -5,17 +5,10 @@ import {
   protectedProcedure,
 } from "~/server/api/trpc";
 
-export const exampleRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
+export const imagesRouter = createTRPCRouter({
 
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+    return ctx.prisma.image.findMany();
   }),
 
   getSecretMessage: protectedProcedure.query(() => {
