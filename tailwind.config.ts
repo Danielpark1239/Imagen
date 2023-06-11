fc4acae2115222ecca7a19c7f442754064d9b40c
Danielpark1239 <Danielpark1239@gmail.com>
2023-06-11 14:13:29 -0400

    Added screen sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,17 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      'xs': '350px',
+      'xss': '390px',
+      's': '430px',
+      'ss': '500px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1560px',
+    },
   },
   plugins: [],
 } satisfies Config;
