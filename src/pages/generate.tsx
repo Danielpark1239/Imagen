7bed9b0804a51ae799e014da9bd02f530dcb230a
Danielpark1239 <Danielpark1239@gmail.com>
2023-06-11 00:56:45 -0400

    Add download/delete buttons, image modal

--- src/pages/generate.tsx
+++ src/pages/generate.tsx
@@ -3,31 +3,60 @@ import Image from "next/image"
 import Head from "next/head"
 import Link from "next/link"
 import { useUser } from "@clerk/nextjs"
+import { Dialog, Transition } from "@headlessui/react"
+import { Fragment, useRef, useState } from "react"
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline"
 import dayjs from "dayjs"
 import relativeTime from "dayjs/plugin/relativeTime"
 import { api } from "~/utils/api"
 import { LoadingSpinner } from "~/components/loading"
-import { useState } from "react"
 import { toast } from "react-hot-toast"
 import Navbar from "~/components/navbar"
 import Footer from "~/components/footer"
+import type { Image as PrismaImage } from "@prisma/client"
 
 dayjs.extend(relativeTime)
 
 const CreateImageWizard = () => {
   const [input, setInput] = useState<string>("")
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false)
+  const [imageModalOpen, setImageModalOpen] = useState(false)
+  const [createdImage, setCreatedImage] = useState<PrismaImage | null | undefined>(undefined)
+  const cancelButtonRef = useRef(null)
+
   const { user } = useUser()
   if (!user) {
     return null
   }
   const ctx = api.useContext()
   const {
-    data: createdImage,
-    mutate,
+    mutate: createMutate,
     isLoading: isGenerating,
   } = api.images.create.useMutation({
-    onSuccess: () => {
+    onSuccess: (createdData) => {
       setInput("")
+      setCreatedImage(createdData)
+      void ctx.images.invalidate()
+    },
+    onError: (e) => {
+      // TRPC Error
+      console.log(e)
+      if (e.message) {
+        toast.error(e.message)
+      } else {
+        // Zod Error
+        console.log(e.data?.zodError)
+        const errorMessage = e.data?.zodError?.fieldErrors.prompt
+        if (errorMessage && errorMessage[0]) {
+          toast.error(errorMessage[0])
+        } else {
+          toast.error(e.message)
+        }
+      }
+    },
+  })
+  const { mutate: deleteMutate } = api.images.delete.useMutation({
+    onSuccess: () => {
       void ctx.images.invalidate()
     },
     onError: (e) => {
@@ -90,14 +119,14 @@ const CreateImageWizard = () => {
               }
             }}
             className="flex h-7 w-24 items-center justify-center rounded-lg border-b-2 border-violet-900
-          bg-violet-700 p-2 font-semibold text-white duration-300 ease-in hover:scale-105 hover:border-violet-800 hover:bg-violet-600"
+            bg-violet-700 p-2 font-semibold text-white duration-300 ease-in hover:scale-105 hover:border-violet-800 hover:bg-violet-600"
           >
             Surprise me
           </button>
         </div>
         <div className="flex w-full flex-row rounded-lg border-b-2 border-slate-400 bg-slate-100 font-semibold text-black">
           <input
-            className="bg-slate-50 text-md flex w-11/12 flex-wrap items-center justify-center rounded-lg p-2"
+            className="text-md flex w-11/12 flex-wrap items-center justify-center rounded-lg bg-slate-50 p-2"
             placeholder="Enter a prompt!"
             value={input}
             onChange={(e) => setInput(e.target.value)}
@@ -105,9 +134,9 @@ const CreateImageWizard = () => {
           ></input>
           {!isGenerating ? (
             <button
-              onClick={() => mutate({ prompt: input })}
+              onClick={() => createMutate({ prompt: input })}
               disabled={isGenerating || input === ""}
-              className="w-1/12 rounded-lg border-l-2 bg-slate-50 disabled:text-slate-300 disabled:bg-slate-50 hover:text-violet-700 duration-300 ease-in"
+              className="w-1/12 rounded-lg border-l-2 bg-slate-50 duration-300 ease-in hover:text-violet-700 disabled:bg-slate-50 disabled:text-slate-300"
             >
               Generate
             </button>
@@ -120,22 +149,173 @@ const CreateImageWizard = () => {
       </div>
       <div className="h-full">
         {createdImage && (
-          <div className="flex flex-row gap-4 rounded-lg border-2 border-slate-300 bg-slate-50 p-4">
-            <Image
-              width={1024}
-              height={1024}
-              className="h-80 w-80 shadow-lg shadow-slate-700 hover:shadow-violet-700 duration-200 ease-in hover:cursor-pointer"
-              src={createdImage.url}
-              alt="Generated image"
-              quality={100}
-            />
-            <div className="flex flex-col px-8">
-              <div className="flex gap-1 font-bold text-slate-300">
-                <span className="font-thin text-black">{`Generated ${dayjs(createdImage.createdAt).fromNow()}`}</span>
+          <>
+            <div className="flex flex-row gap-4 rounded-lg border-2 border-slate-300 bg-slate-50 p-4">
+              <Image
+                width={1024}
+                height={1024}
+                className="h-80 w-80 shadow-lg shadow-slate-700 duration-200 ease-in hover:cursor-pointer hover:shadow-violet-700"
+                src={createdImage.url}
+                alt="Generated image"
+                quality={100}
+                onClick={() => setImageModalOpen(true)}
+              />
+              <div className="flex flex-col px-8">
+                <div className="flex gap-1 font-bold text-slate-300">
+                  <span className="font-thin text-black">{`Generated ${dayjs(
+                    createdImage.createdAt
+                  ).fromNow()}`}</span>
+                </div>
+                <span className="h-5/6 overflow-auto pb-4 font-serif text-xl">
+                  {createdImage.prompt}
+                </span>
+                <div className="flex h-fit content-end items-end justify-start gap-3 align-bottom">
+                  <Link
+                    href={createdImage.url}
+                    className="text-md flex h-9 items-center justify-center rounded-xl border-b-4 border-violet-900
+                      bg-violet-700 px-4 font-medium text-white duration-300 ease-in hover:scale-105 hover:border-violet-800 hover:bg-violet-600"
+                  >
+                    Download
+                  </Link>
+                  <button
+                    className="text-md flex h-9 items-center justify-center rounded-xl border-b-4 border-violet-900
+                      bg-violet-700 px-4 font-medium text-white duration-300 ease-in hover:scale-105 hover:border-violet-800 hover:bg-violet-600"
+                    onClick={() => setDeleteModalOpen(true)}
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
-              <span className="text-xl font-serif overflow-auto">{createdImage.prompt}</span>
+            </div>
+            <Transition appear show={deleteModalOpen} as={Fragment}>
+              <Dialog
+                as="div"
+                className="relative z-10"
+                initialFocus={cancelButtonRef}
+                onClose={() => setDeleteModalOpen(false)}
+              >
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <div className="fixed inset-0 bg-black bg-opacity-25" />
+                </Transition.Child>
+                <div className="fixed inset-0 overflow-y-auto">
+                  <div className="flex min-h-full items-center justify-center p-4 text-center">
+                    <Transition.Child
+                      as={Fragment}
+                      enter="ease-out duration-300"
+                      enterFrom="opacity-0 scale-95"
+                      enterTo="opacity-100 scale-100"
+                      leave="ease-in duration-200"
+                      leaveFrom="opacity-100 scale-100"
+                      leaveTo="opacity-0 scale-95"
+                    >
+                      <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                        <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                          <div className="sm:flex sm:items-start">
+                            <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                              <ExclamationTriangleIcon
+                                className="h-6 w-6 text-red-600"
+                                aria-hidden="true"
+                              />
+                            </div>
+                            <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                              <Dialog.Title
+                                as="h3"
+                                className="text-base font-semibold leading-6 text-gray-900"
+                              >
+                                Delete image
+                              </Dialog.Title>
+                              <div className="mt-2">
+                                <p className="text-sm text-gray-500">
+                                  Are you sure you want to delete this image?
+                                  Image data will be permanently removed. This
+                                  action cannot be undone.
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                          <button
+                            type="button"
+                            className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                            onClick={() => {
+                              deleteMutate({ id: createdImage.id })
+                              setCreatedImage(null)
+                              setDeleteModalOpen(false)
+                            }}
+                          >
+                            Delete
+                          </button>
+                          <button
+                            type="button"
+                            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                            onClick={() => setDeleteModalOpen(false)}
+                            ref={cancelButtonRef}
+                          >
+                            Cancel
+                          </button>
+                        </div>
+                      </Dialog.Panel>
+                    </Transition.Child>
+                  </div>
+                </div>
+              </Dialog>
+            </Transition>
+            <Transition appear show={imageModalOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => setImageModalOpen(false)}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-80" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-3xl">
+                  <div className="bg-slate-200 p-4">
+                    <Image
+                      width={1024}
+                      height={1024}
+                      className="h-full w-full"
+                      src={createdImage.url}
+                      alt="Generated image"
+                      quality={100}
+                    />
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
             </div>
           </div>
+        </Dialog>
+      </Transition>
+          </>
         )}
         <div className="flex justify-center pt-8">
           <Link
