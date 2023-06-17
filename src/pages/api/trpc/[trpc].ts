5eb74d9668208c151b6634b01c92f0ed41df22e2
Danielpark1239 <Danielpark1239@gmail.com>
2023-06-17 19:29:10 -0400

    Try putting trpc on the edge

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,18 +1,32 @@
-import { createNextApiHandler } from "@trpc/server/adapters/next";
-import { env } from "~/env.mjs";
-import { appRouter } from "~/server/api/root";
-import { createTRPCContext } from "~/server/api/trpc";
+import { appRouter } from "~/server/api/root"
+import { createTRPCContext } from "~/server/api/trpc"
+
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
+import type { NextRequest } from 'next/server'
+
+export const config = { // Use Vercel edge runtime since image api sometimes times out
+  runtime: 'edge',
+};
 
 // export API handler
-export default createNextApiHandler({
-  router: appRouter,
-  createContext: createTRPCContext,
-  onError:
-    env.NODE_ENV === "development"
-      ? ({ path, error }) => {
-          console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
-          );
-        }
-      : undefined,
-});
+export default async function handler(req: NextRequest) {
+  return fetchRequestHandler({
+    endpoint: '/api/trpc',
+    router: appRouter,
+    req,
+    createContext: createTRPCContext,
+  });
+}
+
+// export default createNextApiHandler({
+//   router: appRouter,
+//   createContext: createTRPCContext,
+//   onError:
+//     env.NODE_ENV === "development"
+//       ? ({ path, error }) => {
+//           console.error(
+//             `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+//           );
+//         }
+//       : undefined,
+// });
