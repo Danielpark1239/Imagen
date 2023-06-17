277a99f7f0e6ee5130db365c0b68ff0e15e1f485
Danielpark1239 <Danielpark1239@gmail.com>
2023-06-17 19:47:20 -0400

    Revert

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,32 +1,31 @@
 import { appRouter } from "~/server/api/root"
 import { createTRPCContext } from "~/server/api/trpc"
+import { createNextApiHandler } from "@trpc/server/adapters/next";
 
-import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
-import type { NextRequest } from 'next/server'
+// import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
+// import type { NextRequest } from 'next/server'
 
 export const config = { // Use Vercel edge runtime since image api sometimes times out
   runtime: 'edge',
 };
 
-// export API handler
-export default async function handler(req: NextRequest) {
-  return fetchRequestHandler({
-    endpoint: '/api/trpc',
-    router: appRouter,
-    req,
-    createContext: createTRPCContext,
-  });
-}
+// // export API handler
+// export default async function handler(req: NextRequest) {
+//   return fetchRequestHandler({
+//     endpoint: '/api/trpc',
+//     router: appRouter,
+//     req,
+//     createTRPCContext,
+//   });
+// }
 
-// export default createNextApiHandler({
-//   router: appRouter,
-//   createContext: createTRPCContext,
-//   onError:
-//     env.NODE_ENV === "development"
-//       ? ({ path, error }) => {
-//           console.error(
-//             `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
-//           );
-//         }
-//       : undefined,
-// });
+export default createNextApiHandler({
+  router: appRouter,
+  createContext: createTRPCContext,
+  onError:
+    ({ path, error }) => {
+        console.error(
+          `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+        );
+      }
+})
