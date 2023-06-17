f6a37269576cb7c4956e44492398d92c09facce7
Danielpark1239 <Danielpark1239@gmail.com>
2023-06-17 19:54:54 -0400

    revert pt 2

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -5,9 +5,9 @@ import { createNextApiHandler } from "@trpc/server/adapters/next";
 // import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
 // import type { NextRequest } from 'next/server'
 
-export const config = { // Use Vercel edge runtime since image api sometimes times out
-  runtime: 'edge',
-};
+// export const config = { // Use Vercel edge runtime since image api sometimes times out
+//   runtime: 'edge',
+// };
 
 // // export API handler
 // export default async function handler(req: NextRequest) {
