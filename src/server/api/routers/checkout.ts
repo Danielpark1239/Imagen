478fbfef0b2bbabafe0094acffcda4d97f5b231b
Danielpark1239 <Danielpark1239@gmail.com>
2023-06-18 21:20:09 -0400

    fix attempt with stripe

--- src/server/api/routers/checkout.ts
+++ src/server/api/routers/checkout.ts
@@ -2,18 +2,17 @@ import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc"
 import Stripe from "stripe";
 import { TRPCError } from "@trpc/server";
 
-if (!process.env.STRIPE_SECRET_KEY) {
-    throw new Error("Missing Stripe Secret Key")
-}
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2022-11-15",
-});
-
 export const checkoutRouter = createTRPCRouter({
   createCheckout10: protectedProcedure.mutation(async ({ ctx }) => {
     if (!process.env.HOST) {
       throw new Error("Missing Host")
     }
+    if (!process.env.STRIPE_SECRET_KEY) {
+      throw new Error("Missing Stripe Secret Key")
+    }
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+      apiVersion: "2022-11-15",
+    })
     const checkoutSession = await stripe.checkout.sessions.create({
       payment_method_types: ["card", "us_bank_account"],
       metadata: {
@@ -37,6 +36,12 @@ export const checkoutRouter = createTRPCRouter({
     if (!process.env.HOST) {
       throw new Error("Missing Host")
     }
+    if (!process.env.STRIPE_SECRET_KEY) {
+      throw new Error("Missing Stripe Secret Key")
+    }
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+      apiVersion: "2022-11-15",
+    })
     const checkoutSession = await stripe.checkout.sessions.create({
       payment_method_types: ["card", "us_bank_account"],
       metadata: {
@@ -60,6 +65,12 @@ export const checkoutRouter = createTRPCRouter({
     if (!process.env.HOST) {
       throw new Error("Missing Host")
     }
+    if (!process.env.STRIPE_SECRET_KEY) {
+      throw new Error("Missing Stripe Secret Key")
+    }
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+      apiVersion: "2022-11-15",
+    })
     const checkoutSession = await stripe.checkout.sessions.create({
       payment_method_types: ["card", "us_bank_account"],
       metadata: {
