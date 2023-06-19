a301696c5dc6366e7cbd64b91bdb7b37ff3665bc
Danielpark1239 <Danielpark1239@gmail.com>
2023-06-18 22:41:37 -0400

    revert

--- README.md
+++ README.md
@@ -26,3 +26,9 @@ You can check out the [create-t3-app GitHub repository](https://github.com/t3-os
 ## How do I deploy this?
 
 Follow our deployment guides for [Vercel](https://create.t3.gg/en/deployment/vercel), [Netlify](https://create.t3.gg/en/deployment/netlify) and [Docker](https://create.t3.gg/en/deployment/docker) for more information.
+
+# To run (assuming you have the env vars):
+```
+yarn
+yarn run dev
+```
--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,3 +1,37 @@
+<<<<<<< HEAD
+import { appRouter } from "~/server/api/root"
+import { createTRPCContext } from "~/server/api/trpc"
+// import { createNextApiHandler } from "@trpc/server/adapters/next";
+
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
+import type { NextRequest } from 'next/server'
+
+export const config = { // Use Vercel edge runtime since image api sometimes times out
+  runtime: 'edge',
+  unstable_allowDynamic:'*' // just testing
+};
+
+// export API handler
+export default async function handler(req: NextRequest) {
+  return fetchRequestHandler({
+    endpoint: '/api/trpc',
+    router: appRouter,
+    req,
+    createContext: createTRPCContext,
+  });
+}
+
+// export default createNextApiHandler({
+//   router: appRouter,
+//   createContext: createTRPCContext,
+//   onError:
+//     ({ path, error }) => {
+//         console.error(
+//           `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+//         );
+//       }
+// })
+=======
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { env } from "~/env.mjs";
 import { appRouter } from "~/server/api/root";
@@ -16,3 +50,4 @@ export default createNextApiHandler({
         }
       : undefined,
 });
+>>>>>>> parent of 5eb74d9 (Try putting trpc on the edge)
